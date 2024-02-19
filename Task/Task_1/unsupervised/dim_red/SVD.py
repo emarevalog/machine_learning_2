a9b4f3ada5e18ed88f7905482e56dad472c3617f
Melissa <melissa.arevalog8@gmail.com>
2024-02-18 23:47:52 -0500

    update lab1

--- Task/Task_1/unsupervised/dim_red/SVD.py
+++ Task/Task_1/unsupervised/dim_red/SVD.py
@@ -1,26 +1,52 @@
 import numpy as np
 
 class SVD:
-    def __init__(self, n_components=None):
-        self.n_components = n_components
-        self.s = None
-        self.Vt = None
+    def __init__(self, nsing_vals:int=-1):
+        self.nsing_vals = nsing_vals
+        self.U = None
+        self.Sigma = None
+        self.V_T = None
+    
+    def fit(self, A):
+        """
+        Compute the Singular Value Decomposition (SVD) of a matrix A.
 
-    def fit(self, X):
-        covariance_matrix = np.cov(X, rowvar=False)
-        eigen_values, eigen_vectors = np.linalg.eigh(covariance_matrix)
-        sorted_indices = np.argsort(eigen_values)[::-1]
-        sorted_eigen_values = eigen_values[sorted_indices]
-        sorted_eigen_vectors = eigen_vectors[:, sorted_indices]
-        if self.n_components is not None:
-            sorted_eigen_values = sorted_eigen_values[:self.n_components]
-            sorted_eigen_vectors = sorted_eigen_vectors[:, :self.n_components]
-        self.s = np.sqrt(sorted_eigen_values)
-        self.Vt = sorted_eigen_vectors.T
+        Parameters:
+        A (ndarray): Input matrix of shape (m, n).
+        """
+        # Centralizing data
+        mean = np.mean(A, axis=0)
+        #A = A - mean
 
-    def transform(self, X):
-        return np.dot(X, self.Vt.T)
+        # Computing left singular vectors
+        B = np.dot(A, A.T)
+        eigval_U, eigvec_U = np.linalg.eigh(B)
+        ncols = np.argsort(eigval_U)[::-1]
+        self.U = eigvec_U[:, ncols]
 
-    def fit_transform(self, X):
-        self.fit(X)
-        return np.dot(X, self.Vt.T) / self.s
+        # Computing right singular vectors
+        B = np.dot(A.T, A)
+        eigval_VT, eigvec_VT = np.linalg.eigh(B)
+        ncols = np.argsort(eigval_VT)[::-1]
+        self.V_T = eigvec_VT[:,ncols].T
+
+        # Computing singular values
+        if (np.size(np.dot(A, A.T)) > np.size(np.dot(A.T, A))): 
+            B = np.dot(A.T, A) 
+        else: 
+            B = np.dot(A, A.T)
+        eigval_sigma, _ = np.linalg.eigh(B)
+        eigval_sigma = np.sqrt(np.abs(eigval_sigma))
+        self.Sigma = np.sort(eigval_sigma)[::-1]
+    
+    def transform(self):
+        # Recovering matrix A with nsing_vals
+        if self.nsing_vals != -1:
+            A_transformed = self.U[:, :self.nsing_vals] @ np.diag(self.Sigma[:self.nsing_vals]) @ self.V_T[:self.nsing_vals, :]
+        else:
+            A_transformed = self.U @ np.diag(self.Sigma) @ self.V_T
+        return A_transformed
+    
+    def fit_transform(self, A):
+        self.fit(A)
+        return self.transform()
